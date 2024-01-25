18c3145447b9e2aeef06c1f57055a5c356ed49ac
Yohanens Mulu <dev.yohannes@gmail.com>
2024-01-25 14:48:44 +0300

    adding click event listener for the bars

---
+++ script.js
@@ -0,0 +1,15 @@
+const bars = document.querySelectorAll(".bar");
+const moneySpan = document.querySelectorAll(".money-value");
+
+moneySpan.forEach((money) => {
+  money.classList.toggle("money-display");
+});
+
+bars.forEach((bar) => {
+  bar.addEventListener("click", () => {
+    moneySpan.forEach((money, e) => {
+      //   money.classList.toggle("money-display");
+      console.log(e.targe);
+    });
+  });
+});
